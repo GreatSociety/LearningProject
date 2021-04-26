8177aeeb8c11c715e1d233b6f166ae27d7102300
GreatSociety <greatsociety17@gmail.com>
2021-04-26 15:01:22 +0300

    next iter_14

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,18 +15,6 @@ public class GameManager : MonoBehaviourPunCallbacks
     private void Start()
     {
         gameManager = this;
-
-        if (playerPrefab == null)
-        {
-            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
-        }
-        else
-        {
-            print("ye");
-            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-            DontDestroyOnLoad(PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(22f, 1.8f, 32f), Quaternion.identity, 0));
-            DontDestroyOnLoad(this.gameObject);
-        }
     }
 
     /// <summary>
@@ -45,9 +33,10 @@ public class GameManager : MonoBehaviourPunCallbacks
         if (PhotonNetwork.IsMasterClient)
         {
             Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
-
             LoadArena();
         }
+
+        DontDestroyOnLoad(PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(22f, 1.8f, 32f), Quaternion.identity, 0));
     }
 
     public override void OnPlayerLeftRoom(Player other)
