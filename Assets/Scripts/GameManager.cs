3d27b87979bee79ca2eb94501579a405c8a5b6fb
GreatSociety <greatsociety17@gmail.com>
2021-04-26 14:41:00 +0300

    Next Try to sync

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,24 @@ using Photon.Realtime;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    [Tooltip("The prefab to use for representing the player")]
+    public GameObject playerPrefab;
 
     public static GameManager gameManager;
 
     private void Start()
     {
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+        }
+        else
+        {
+            Debug.LogFormat("We are Instantiating LocalPlayer from {0}");
+            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(22f, 1.8f, 32f), Quaternion.identity, 0);
+        }
+
         gameManager = this;
     }
 
