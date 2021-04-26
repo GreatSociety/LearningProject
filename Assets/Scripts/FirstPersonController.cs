62d02fc042f7810cd24d498c4f8aa924a15793bc
GreatSociety <greatsociety17@gmail.com>
2021-04-26 16:08:46 +0300

    And next iter

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
-public class FirstPersonController : MonoBehaviourPunCallbacks
+public class FirstPersonController : MonoBehaviourPunCallbacks, IPunObservable
 {
     [SerializeField] Camera firstPersonCam;
 
