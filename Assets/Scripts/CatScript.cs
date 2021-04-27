58538cee0a46adce1421a0050550ace6d47cdf4e
valeria <valeriyagaro@gmail.com>
2021-04-28 01:38:23 +0300

    cat

--- Assets/Scripts/CatScript.cs
+++ Assets/Scripts/CatScript.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class CatScript : MonoBehaviour
 {
     // Start is called before the first frame update
+   private  Vector3 Endpoint;
+   private int speed = 10; 
+   
+
     void Start()
     {
         
@@ -13,6 +17,20 @@ public class CatScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (SmatphoneInputControl.IsAllowed)
+        {
+            GetComponent<Animator>().SetBool("walk", true);
+            Endpoint = new Vector3(42.0f, 0.1f, 28.0f);
+            transform.position = Vector3.MoveTowards(transform.position, Endpoint, Time.deltaTime * speed);
+        }
     }
+    void Miau()
+    {
+        if (SafeScript.Opened) 
+        {
+            GetComponent<Animator>().SetBool("cat_armature|miau", true);
+        }
+       
+    }
+   
 }
